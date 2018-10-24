6320af7143088a834c796038903560ee1b6bfbba
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-10-25 01:20:07 +0300

    HW 3

---
+++ BoolAndCow - HW 3/jersey-rest-homework/src/main/java/org/elsys/netprog/rest/Game.java
@@ -0,0 +1,69 @@
+package org.elsys.netprog.rest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import org.json.simple.JSONObject;
+
+public class Game {
+	private String gameId = "";
+	private int cowsN, bullsN;
+	private int turnsCount;
+	private boolean success;
+	private int secret = 0;
+	private int secretL;
+	public Game(int gameId, int secretL) {
+		Random rand = new Random();
+		this.gameId = "" + gameId;
+		this.cowsN = 0;
+		this.bullsN = 0;
+		this.turnsCount = 0;
+		this.success = false;
+		this.secretL = secretL;
+		ArrayList<Integer> pool = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+		for(int i = 0; i < secretL; i++) {
+			int id =  rand.nextInt(pool.size() - 1);
+			this.secret = this.secret * 10 + pool.remove(id);
+		}
+	}
+	
+	public String getGameId() {
+		return gameId;
+	}
+	
+	public void tryGuess(String guess) {
+		turnsCount++;
+		bullsN = 0;
+		cowsN = 0;
+		String targetS = Integer.toString(secret);
+		for(int i = 0; i < secretL; i++) {
+			if(guess.charAt(i) == targetS.charAt(i)) {
+				bullsN++;
+			}else if(targetS.contains(guess.charAt(i) + "")){
+				cowsN++;
+			}
+		}
+		if(bullsN == secretL) {
+			success = true;
+		}
+	}
+	
+	public JSONObject json() {
+		JSONObject obj = new JSONObject();
+		obj.put("gameId", gameId);
+		obj.put("cowsNumber", cowsN);
+		obj.put("bullsNumber", bullsN);
+		obj.put("turnsCount", turnsCount);
+		obj.put("success", success);
+		return obj;
+	}
+	
+	public JSONObject jsonArray() {
+		JSONObject obj = new JSONObject();
+		obj.put("gameId", gameId);
+		obj.put("turnsCount", turnsCount);
+		obj.put("secret", success ? Integer.toString(secret) : "****");
+		obj.put("success", success);
+		return obj;
+	}
+}
---
+++ BoolAndCow - HW 3/jersey-rest-homework/src/main/java/org/elsys/netprog/rest/GameController.java
@@ -0,0 +1,100 @@
+package org.elsys.netprog.rest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.elsys.netprog.rest.Game;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/game")
+public class GameController {
+	static ArrayList<Game> games = new ArrayList<Game>();
+	@POST
+	@Path("/startGame")
+	@Produces(value={MediaType.APPLICATION_JSON})
+	public Response startGame() throws URISyntaxException{
+		//TODO: Add your code here
+		Game g = new Game(games.size() == 0 ? 0 : games.size(), 4);
+		games.add(g);
+		return Response.created(new URI("/games")).entity(g.getGameId()).build();
+	}
+	
+	@PUT
+	@Path("/guess/{id}/{guess}")
+	@Produces(value={MediaType.APPLICATION_JSON})
+	public Response guess(@PathParam("id") String gameId, @PathParam("guess") String guess) throws Exception{
+		int status = 404;
+		Game currGame = null;
+		JSONObject JSONResult = null;
+		for(Game g : games) {
+			System.out.println(g.getGameId().equals(gameId));
+			System.out.println(g.getGameId() + " " + gameId);
+			if(g.getGameId().equals(gameId)) {
+				status = 200;
+				currGame = g;
+			}
+		}
+		int guessI = 0;
+		try {
+			guessI = Integer.parseInt(guess);
+			if(checkDupes(guessI) || guessI < 1000 || guessI > 9999) {
+				status = 400;
+			}
+		}catch(Exception e) {
+			status = 400;
+		}
+		if(status == 200) {
+			currGame.tryGuess(guess);
+			JSONParser parser = new JSONParser();
+			JSONResult = currGame.json();
+		}
+		return Response.status(status).entity(JSONResult).build();
+	}
+	
+	@GET
+	@Path("/games")
+	@Produces(value={MediaType.APPLICATION_JSON})
+	public Response getGames(){
+		JSONArray allGames  = new JSONArray();
+		for(Game g : games) {
+			try {
+				allGames.add(g.jsonArray());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return Response.status(200).entity(allGames).build();
+	}
+	
+	public boolean checkDupes(int n) { 
+		boolean[] digits = new boolean[10];
+		int i;
+		while(n > 0) {
+			i = n%10;
+			if(digits[i])
+				return true;
+			digits[i] = true;
+			n /= 10;
+		}
+		return false;
+	}
+	
+}
