7ecd2ffadc8166c7d73878af3f33ff029e35fc27
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-12-06 01:29:39 +0200

    added 4-th homework

---
+++ 4/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /Internet|Words|Or/,
+"exercise_2": /08{2}[1-9]{7}/,
+"exercise_3": /[^0-1]+/,
+"exercise_4": /^[A-Za-z][\w.]{2,14}$/,
+"exercise_5": /^(?!1501|2000)[0-9]{3,4}$/,
+"exercise_6": /(class=(\"\D+\"|\'\D+\'))/
+};
+cssSelectors = {
+"exercise_1": " item > java ",
+"exercise_2": " different > .diffClass ",
+"exercise_3": " java > tag.class1.class2",
+"exercise_4": " css > item:nth-child(3) ",
+"exercise_5": " tag > java:nth-child(2) ",
+"exercise_6": " item.class2 > item.class1, item.class1 > item.class2 ",
+"exercise_7": " different#diffId2 > java:last-child ",
+"exercise_8": " css > #someId "
+};
