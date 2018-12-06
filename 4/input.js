d24a1f2aef01dc040d43067884dd16b0cd815bb2
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-12-06 21:10:43 +0200

    some revisions

--- 4/input.js
+++ 4/input.js
@@ -1,14 +1,14 @@
 regExps = {
 "exercise_1": /Internet|Words|Or/,
-"exercise_2": /08{2}[1-9]{7}/,
+"exercise_2": /08{2}[1-7]{7}/,
 "exercise_3": /[^0-1]+/,
 "exercise_4": /^[A-Za-z][\w.]{2,14}$/,
 "exercise_5": /^(?!1501|2000)[0-9]{3,4}$/,
-"exercise_6": /(class=(\"\D+\"|\'\D+\'))/
+"exercise_6": /class=(\"\D+\"|\'\D+\')/
 };
 cssSelectors = {
-"exercise_1": " item > java ",
-"exercise_2": " different > .diffClass ",
+"exercise_1": " item > java.class1 ",
+"exercise_2": " different > java.diffClass ",
 "exercise_3": " java > tag.class1.class2",
 "exercise_4": " css > item:nth-child(3) ",
 "exercise_5": " tag > java:nth-child(2) ",
